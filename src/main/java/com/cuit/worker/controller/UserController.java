00b84d5a8118d1c7cec3b93088a3075e734b35eb
ljl <1274042829@qq.com>
2018-12-12 00:30:01 +0800

    基础前后交互

--- src/main/java/com/cuit/worker/controller/UserController.java
+++ src/main/java/com/cuit/worker/controller/UserController.java
@@ -3,11 +3,16 @@ package com.cuit.worker.controller;
 
 import com.cuit.worker.model.User;
 import com.cuit.worker.service.UserService;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -16,11 +21,20 @@ public class UserController {
     private UserService userService;
 
     @RequestMapping("/list")
-    public void findAll(){
-        List<User> userEntities = userService.findAllUser();
-        for (User user: userEntities
+    public ResponseEntity<List<User>> findAll(){
+        List<User> users = userService.findAllUser();
+        for (User user: users
              ) {
             System.out.println(user.getUsername());
         }
+        Optional<User> user = userService.findByUserId(1);
+        return ResponseEntity.ok(users);
+    }
+    @RequestMapping(value = "/user/login",method = RequestMethod.POST)
+    public ResponseEntity<User> Login(@RequestBody User user){
+            if (user.getUsername()!=null&&user.getPassword()!=null){
+                    return ResponseEntity.ok(user);
+            }
+            return ResponseEntity.ok(user);
     }
 }
--- src/main/java/com/cuit/worker/service/UserService.java
+++ src/main/java/com/cuit/worker/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +16,8 @@ public class UserService {
 
     public List<User> findAllUser() {
         return userRepository.findAll();
+    }
+    public Optional<User> findByUserId(Integer id){
+        return userRepository.findById(id);
     }
 }
